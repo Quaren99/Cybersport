73cc5ed798fb78dc4526316306267a4ad505b2d6
Quaren99 <semenukr39@gmail.com>
2025-06-07 19:02:48 +0300

    seeds fix

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,6 +2,6 @@ class Participant < ApplicationRecord
   belongs_to :tournament
   belongs_to :team
 
-  validates :place, presence: true, uniqueness: { scope: :tournament_id }, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
-  validates :prize, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :place, uniqueness: { scope: :tournament_id }, allow_nil: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :prize, allow_nil: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,25 @@ end
 def create_team(worldRanking)
   Team.create!(
     name: Faker::Sports::Football.unique.team,
+    description: Faker::Lorem.sentence(word_count: 10),
     worldRanking: worldRanking
-  )
-end
-def create_member
-  joined_date = Faker::Date.between(from: 10.years.ago, to: Date.today)
-  left_date = nil
-
-  if [ true, false ].sample && rand > 0.4
-    left_date = Faker::Date.between(from: joined_date + 1.month, to: joined_date + 8.years)
-  end
-  Member.create!(
-    player: Player.all.sample,
-    team: Team.all.sample,
-    joined: joined_date,
-    left: left_date
   )
 end
 
 def create_tournament
   prizepool = rand(1000..1000000)
+  date = Faker::Date.between(from: 6.year.ago, to: Date.today + 1.year)
   t = Tournament.create!(
       name: Faker::Sports::Football.unique.competition,
-      date: Faker::Date.between(from: 6.year.ago, to: Date.today),
+      date: date,
       prizepool: prizepool
     )
     8.times do |i|
-      create_participant(i + 1, prizepool / ((i + 1)* 2), t)
+      if date > Date.today
+        create_participant(nil, nil, t)
+      else
+        create_participant(i + 1, prizepool / ((i + 1)* 2), t)
+      end
     end
 end
 
@@ -47,6 +39,29 @@ def create_participant(place, prize, tournament)
     )
 end
 
+def create_team_members(team)
+  team_start = Faker::Date.between(from: 10.years.ago, to: 5.years.ago)
+  team_end = Date.today + 1.year
+
+  5.times do
+    current_start = team_start
+    while current_start < team_end
+      min_left = current_start + 6.months
+      max_left = [ current_start + 3.years, team_end ].min
+      left = [ true, false ].sample ? Faker::Date.between(from: min_left, to: max_left) : nil
+      left = nil if left && left > team_end
+      Member.create!(
+        player: Player.all.sample,
+        team: team,
+        joined: current_start,
+        left: left
+      )
+      break unless left
+      current_start = left
+    end
+  end
+end
+
 100.times do
   create_player
 end
@@ -55,8 +70,8 @@ end
   create_team(i + 1)
 end
 
-200.times do
-  create_member
+Team.all.each do |team|
+  create_team_members(team)
 end
 
 20.times do
